4b290c73aa9e59f6196da22f47722b9c462f958e
Nelson Bernard <nelsonbernardiv@gmail.com>
2018-01-09 13:59:32 -0500

    Add files via upload

---
+++ Signature Save/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Signature_Save
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnNewCapture = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblInitials = new System.Windows.Forms.Label();
+            this.btnEditDocument = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(401, 91);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnNewCapture
+            // 
+            this.btnNewCapture.Location = new System.Drawing.Point(164, 133);
+            this.btnNewCapture.Name = "btnNewCapture";
+            this.btnNewCapture.Size = new System.Drawing.Size(122, 43);
+            this.btnNewCapture.TabIndex = 1;
+            this.btnNewCapture.Text = "New Capture";
+            this.btnNewCapture.UseVisualStyleBackColor = true;
+            this.btnNewCapture.Click += new System.EventHandler(this.btnNewCapture_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(290, 133);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(122, 43);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Client Name";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(102, 10);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(312, 22);
+            this.textName.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(61, 133);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(95, 43);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblInitials
+            // 
+            this.lblInitials.AutoSize = true;
+            this.lblInitials.Location = new System.Drawing.Point(8, 143);
+            this.lblInitials.Name = "lblInitials";
+            this.lblInitials.Size = new System.Drawing.Size(47, 17);
+            this.lblInitials.TabIndex = 6;
+            this.lblInitials.Text = "Initials";
+            // 
+            // btnEditDocument
+            // 
+            this.btnEditDocument.Location = new System.Drawing.Point(164, 182);
+            this.btnEditDocument.Name = "btnEditDocument";
+            this.btnEditDocument.Size = new System.Drawing.Size(122, 43);
+            this.btnEditDocument.TabIndex = 7;
+            this.btnEditDocument.Text = "Edit Document...";
+            this.btnEditDocument.UseVisualStyleBackColor = true;
+            this.btnEditDocument.Click += new System.EventHandler(this.btnEditDocument_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(424, 233);
+            this.Controls.Add(this.btnEditDocument);
+            this.Controls.Add(this.lblInitials);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNewCapture);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Signature Saver";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnNewCapture;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblInitials;
+        private System.Windows.Forms.Button btnEditDocument;
+    }
+}
+
---
+++ Signature Save/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Ink;
+using System.IO;
+using Word = Microsoft.Office.Interop.Word;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Signature_Save
+{
+    public partial class Form1 : Form
+    {
+        
+        private InkOverlay inkOverlay;
+        private InkOverlay initialsOverlay;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeOverlays();
+            Word.Application WordApp;                     
+        }
+
+        private void btnNewCapture_Click(object sender, EventArgs e)
+        {
+            ClearOverlays();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            inkOverlay.Enabled = false;
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            string fileName = filePath + textName.Text;
+            string initialsFileName = filePath + textName.Text + " - Initials";
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = textName.Text;
+            sfd.Filter = "GIF Images (*.gif)|*.gif";
+            sfd.InitialDirectory = filePath;
+
+
+            if (inkOverlay.Ink.Strokes.Count > 0)
+            {
+                if (textName.Text != "")
+                {
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+                        Rectangle signatureBounds = this.pictureBox1.Bounds;
+                        inkOverlay.Ink.Clip(signatureBounds);
+                        byte[] bytes = (byte[])inkOverlay.Ink.Save(PersistenceFormat.Gif, CompressionMode.NoCompression);
+
+                        using (MemoryStream ms = new MemoryStream(bytes))
+                        {
+                            using (Bitmap gif = new Bitmap(ms))
+                            {
+                                gif.Save(sfd.FileName);                                
+                            }
+                        }
+                    }
+                }
+                else
+                    MessageBox.Show("Signature must be attached to a client name");
+            }
+            else
+            {
+                MessageBox.Show("There is no signature to save");
+                this.pictureBox1.Invalidate();
+                inkOverlay.Enabled = true;
+            }
+
+        }
+
+        private void btnEditDocument_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.InitialDirectory = filePath;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string sourceFile = ofd.FileName;
+                    string sourceFilePath = Path.GetDirectoryName(sourceFile);
+                    UpdateDoc(sourceFile);            
+                }         
+                
+                       
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Error in process.", "Internal Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /*private void FindAndReplace(Word.Application wordApp, object findText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+
+            byte[] signature = (byte[])inkOverlay.Ink.Save(PersistenceFormat.Gif, CompressionMode.NoCompression);
+            byte[] initials = (byte[])initialsOverlay.Ink.Save(PersistenceFormat.Gif, CompressionMode.NoCompression);
+
+            using (MemoryStream ms = new MemoryStream(signature))
+            {
+                Bitmap gif = new Bitmap(ms);
+                gif.Save(ms, ImageFormat.Png);
+                var data = new DataObject("PNG", ms);
+                Clipboard.Clear();
+                Clipboard.SetDataObject(data, true);
+
+                wordApp.Selection.Find.Execute(ref findText, ref matchCase,
+                ref matchWholeWord, ref matchWildCards, ref matchSoundsLike,
+                ref matchAllWordForms, ref forward, ref wrap, ref format,
+                Clipboard.GetDataObject(), ref replace, ref matchKashida, ref matchDiacritics,
+                ref matchAlefHamza, ref matchControl);
+            }
+        }*/
+        
+        private void InitializeOverlays()
+        {
+            inkOverlay = new InkOverlay();
+            inkOverlay.Handle = this.pictureBox1.Handle;
+            inkOverlay.DefaultDrawingAttributes.AntiAliased = true;
+            inkOverlay.DefaultDrawingAttributes.IgnorePressure = false;
+            inkOverlay.DefaultDrawingAttributes.PenTip = PenTip.Ball;
+            inkOverlay.Enabled = true;
+
+
+            initialsOverlay = new InkOverlay();
+            initialsOverlay.Handle = this.pictureBox2.Handle;
+            initialsOverlay.DefaultDrawingAttributes.AntiAliased = true;
+            initialsOverlay.DefaultDrawingAttributes.IgnorePressure = false;
+            initialsOverlay.DefaultDrawingAttributes.PenTip = PenTip.Ball;
+            initialsOverlay.Enabled = true;
+            Rectangle initialsBounds = this.pictureBox2.Bounds;
+        }
+
+        private void ClearOverlays()
+        {
+            inkOverlay.Enabled = false;
+            initialsOverlay.Enabled = false;
+            inkOverlay.Ink.DeleteStrokes();
+            initialsOverlay.Ink.DeleteStrokes();
+            this.pictureBox2.Invalidate();
+            this.pictureBox1.Invalidate();
+            inkOverlay.Enabled = true;
+            initialsOverlay.Enabled = true;
+        }
+
+        /*private void FindAndReplace()
+        {
+            inkOverlay.Enabled = false;
+            string filePath = Path.GetTempPath();
+            string fileName = filePath + textName.Text + ".gif";
+            string initialsFileName = filePath + textName.Text + " - Initials.gif";
+
+            inkOverlay.Ink.ClipboardPaste();
+            byte[] signature = (byte[])inkOverlay.Ink.Save(PersistenceFormat.Gif, CompressionMode.NoCompression);
+            byte[] initials = (byte[])initialsOverlay.Ink.Save(PersistenceFormat.Gif, CompressionMode.NoCompression);
+
+            using (MemoryStream ms = new MemoryStream(signature))
+            {
+                /*Bitmap gif = new Bitmap(ms);
+                gif.Save(ms, ImageFormat.Png);
+                var data = new DataObject("PNG", ms);
+                Clipboard.Clear();
+                Clipboard.SetDataObject(data, true);
+                
+                using (Bitmap gif = new Bitmap(ms))
+                {
+                    gif.Save(fileName);
+                }
+            }
+
+            using (MemoryStream ms = new MemoryStream(initials))
+            {
+                using (Bitmap gif = new Bitmap(ms))
+                {
+                    gif.Save(initialsFileName);
+                }
+            }
+
+            string searchTerm = "<Signature>";
+            string searchTerm2 = "<Initials>";
+
+            var sel = wordApp.Selection;
+            sel.Find.Text = searchTerm;
+            
+            sel.Find.Execute(Word.WdReplace.wdReplaceAll);
+            
+            sel.InlineShapes.AddPicture(fileName, false, true);
+
+            sel.Find.Text = searchTerm2;
+            sel.Range.Select();
+            sel.Find.Execute(Word.WdReplace.wdReplaceAll);
+            
+            sel.InlineShapes.AddPicture(initialsFileName, false, true);
+        }*/
+
+        public void UpdateDoc(string source)
+        {
+            Word.Application WordApp = new Word.Application();
+            object missing = System.Reflection.Missing.Value;
+            object yes = true;
+            object no = false;
+            Word.Document d;
+            object filename = Path.GetFullPath(source);
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            string fileName = filePath + textName.Text + ".gif";
+            string initialsFileName = filePath + textName.Text + " - Initials.gif";
+
+            d = WordApp.Documents.Open(ref filename, ref missing, ref no, ref missing,
+                   ref missing, ref missing, ref missing, ref missing, ref missing,
+                   ref missing, ref missing, ref yes, ref missing, ref missing, ref missing, ref missing);
+
+            d.Activate();
+            List<Word.Range> ranges = new List<Microsoft.Office.Interop.Word.Range>();
+
+            foreach (Word.Range rngstory in d.StoryRanges)
+            {
+                if (rngstory.Text == "<Signature>")
+                {
+                    ranges.Add(rngstory);
+                    rngstory.Delete();
+                }
+            }
+            foreach (Word.Range r in ranges)
+            {
+                r.InlineShapes.AddPicture(fileName, ref missing, ref missing, ref missing);
+            }
+            //WordApp.Quit(ref yes, ref missing, ref missing);
+        }
+
+    }
+}
